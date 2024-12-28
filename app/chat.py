8d9ac50e38a023bb030117fbbfebb393e93020de
Agos95 <agostini.federico.95@gmail.com>
2024-12-28 12:27:01 +0100

    save messages to db

--- app/chat.py
+++ app/chat.py
@@ -8,7 +8,7 @@ from fastapi.responses import StreamingResponse
 from sqlmodel import Session, select
 
 from .schema import ChatMessageSchema
-from .database.database import get_session, Chat, ChatMessage
+from .database import database as db
 
 logger = logging.getLogger("uvicorn.error")
 
@@ -17,54 +17,90 @@ router = APIRouter(prefix="/chats", tags=["chat"])
 LETTERS = list(string.ascii_letters + string.digits)
 
 
+async def get_response(message: str, chat_id: str, session: Session):
+    # here you would call your LLM to get the response
+    response = ""
+    await asyncio.sleep(2)
+    for _ in range(random.randint(50, 100)):
+        stream = "".join(random.choices(LETTERS, k=5))
+        # logger.info(f"{response}")
+        response += stream
+        yield stream
+        await asyncio.sleep(0.075)
+
+    messages = [
+        db.ChatMessage(chat_id=chat_id, role=db.ChatMessageRole.HUMAN, content=message),
+        db.ChatMessage(chat_id=chat_id, role=db.ChatMessageRole.AI, content=response),
+    ]
+    logger.info("Addiding messages to db session")
+    for m in messages:
+        session.add(m)
+    logger.info("Committing messages to db")
+    session.commit()
+
+
 @router.get("")
-async def get_chats(session: Session = Depends(get_session)) -> list[Chat]:
+async def get_chats(session: Session = Depends(db.get_session)) -> list[db.Chat]:
     """Get list of Chats."""
-    chats = session.exec(select(Chat)).all()
+    chats = session.exec(select(db.Chat)).all()
     return chats
 
 
 @router.get("/{chat_id}")
-async def get_chat(chat_id: str, session: Session = Depends(get_session)) -> Chat:
+async def get_chat(chat_id: str, session: Session = Depends(db.get_session)) -> db.Chat:
     """Get single chat (without messages)"""
-    chat = session.exec(select(Chat).where(Chat.id == chat_id)).one()
+    chat = session.exec(select(db.Chat).where(db.Chat.id == chat_id)).one()
     return chat
 
 
+@router.get("/{chat_id}/messages", tags=["chat", "message"])
+async def get_chat_messages(
+    chat_id: str, session: Session = Depends(db.get_session)
+) -> list[db.ChatMessage]:
+    """Get chat messages"""
+    messages = session.exec(
+        select(db.ChatMessage)
+        .where(db.ChatMessage.chat_id == chat_id)
+        .order_by(db.ChatMessage.create_time)
+    ).all()
+    return messages
+
+
 @router.delete("/{chat_id}")
-async def delete_chat(chat_id: str, session: Session = Depends(get_session)):
+async def delete_chat(chat_id: str, session: Session = Depends(db.get_session)):
     """Delete chat"""
-    chat = session.exec(select(Chat).where(Chat.id == chat_id)).one()
+    chat = session.exec(select(db.Chat).where(db.Chat.id == chat_id)).one()
     session.delete(chat)
     session.commit()
     return
 
 
-@router.get("/{chat_id}/messages", tags=["chat", "message"])
-async def get_chat_messages(
-    chat_id: str, session: Session = Depends(get_session)
-) -> list[ChatMessage]:
-    messages = session.exec(
-        select(ChatMessage)
-        .where(ChatMessage.chat_id == chat_id)
-        .order_by(ChatMessage.create_time)
-    ).all()
-    return messages
-
-
 @router.post("/{chat_id}")
-async def chat(chat_id: str, message: ChatMessageSchema) -> StreamingResponse:
+async def chat(
+    chat_id: str, message: ChatMessageSchema, session: Session = Depends(db.get_session)
+) -> ChatMessageSchema:
+    """Post a message to chat and get the response"""
+    response = "".join(
+        [
+            chunk
+            for chunk in get_response(
+                message=message.message, chat_id=chat_id, session=session
+            )
+        ]
+    )
+    return response
+
+
+@router.post("/{chat_id}/stream")
+async def stream(
+    chat_id: str, message: ChatMessageSchema, session: Session = Depends(db.get_session)
+) -> StreamingResponse:
+    """Post a message to chat and get the response as stream"""
+
     logger.info(f"Streaming chat {chat_id}")
     logger.info(f"Message: {message.message}")
 
-    # msg = message.message
-    # here you would call your LLM in a real example
-    async def streaming():
-        await asyncio.sleep(2)
-        for _ in range(random.randint(50, 100)):
-            response = "".join(random.choices(LETTERS, k=5))
-            logger.info(f"{response}")
-            yield response
-            await asyncio.sleep(0.075)
-
-    return StreamingResponse(streaming(), media_type="text/event-stream")
+    return StreamingResponse(
+        get_response(message=message.message, chat_id=chat_id, session=session),
+        media_type="text/event-stream",
+    )
--- streamlit/main.py
+++ streamlit/main.py
@@ -34,6 +34,7 @@ with st.sidebar:
     for chat in st.session_state.chats.values():
         st.button(
             chat.title,
+            type="secondary",
             use_container_width=True,
             on_click=select_chat,
             kwargs={"chat_id": chat.id},
@@ -59,6 +60,7 @@ if st.session_state.history is not None:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+
 # React to user input
 prompt = st.chat_input("How can I help you?")
 if prompt:
@@ -66,18 +68,17 @@ if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.history.append({"role": "user", "content": prompt})
 
 
 def streaming(chat_id: str, message: str):
-    with httpx.Client() as client:
-        with client.stream(
-            "POST",
-            f"http://localhost:8000/chats/{chat_id}",
-            json={"message": message},
-        ) as r:
-            for chunk in r.iter_text():
-                yield chunk
+    with client.stream(
+        "POST",
+        f"/chats/{chat_id}/stream",
+        json={"message": message},
+    ) as r:
+        for chunk in r.iter_text():
+            yield chunk
 
 
 # Response
@@ -87,4 +88,4 @@ if prompt:
         stream = streaming(st.session_state.chat.id, prompt)
         response = st.write_stream(stream)
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.history.append({"role": "assistant", "content": response})
